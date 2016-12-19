240061914fdcd18c28197f8970d742409bbfbf0c
Yoonkwan Kim <kyk0704@hotmail.com>
2016-12-18 22:10:05 -0500

    updated

--- algorithm/sorting/insertion/InsertionSort.java
+++ algorithm/sorting/insertion/InsertionSort.java
@@ -1,10 +1,10 @@
-public class InsertSort {
+public class InsertionSort {
 
     public static void main(String[] args) {
 
     }
 
-    public static int insertSort(int[] arr, int count) {
+    public static int insertionSort(int[] arr, int count) {
 
         for (int i = 1; i < arr.length; i++) {
             int p = arr[i];
