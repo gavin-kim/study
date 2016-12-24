0b66f7e5bd74d4c23babec6e0ec73a883a986070
Yoonkwan Kim <kyk0704@hotmail.com>
2016-12-24 01:22:02 -0500

    updated

--- algorithm/sorting/merge/MergeSort.java
+++ algorithm/sorting/merge/MergeSort.java
@@ -4,13 +4,14 @@ import java.util.stream.Stream;
 
 public class MergeSort {
 
+
     public static void main(String[] args) {
 
         int[] arr = new int[]{6, 2, 3, 4, 5, 9, 3, 2, 1, 0, 1, 9};
 
         long start = System.nanoTime();
 
-        mergeSort(arr);
+        mergeSortBU(arr);
 
         long end = System.nanoTime();
 
@@ -18,22 +19,40 @@ public class MergeSort {
         System.out.println((end - start) + "ns");
     }
 
-    public static void mergeSort(int[] arr) {
-        sort(arr, 0, arr.length -1);
+    // Top Down merge sort
+    public static void mergeSortTD(int[] arr) {
+        sortTD(arr, 0, arr.length -1);
     }
 
-    public static void sort(int[] arr, int lo, int hi) {
+    private static void sortTD(int[] arr, int lo, int hi) {
         if (lo < hi) {
             int mid = (lo + hi) / 2;
-            sort(arr, lo, mid);
-            sort(arr, mid + 1, hi);
+            sortTD(arr, lo, mid);
+            sortTD(arr, mid + 1, hi);
             merge(arr, lo, mid, hi);
+        }
+    }
+
+    // Bottom Up merge sort
+    public static void mergeSortBU(int[] arr) {
+        sortBU(arr);
+    }
+
+    private static void sortBU(int[] arr) {
+        int n = arr.length;
+
+        // size: 1  (1:1) ... (1:1) ...
+        // size: 2  (2:2) ... (2:2) ...
+        for (int size = 1; size < n; size *= 2) {    // size: subarray size  (1, 2, 4, 8 ....)
+            for (int i = 0; i < n; i += size * 2) {  // i:    subarray index
+                merge(arr, i, i + size - 1 , Math.min(i + size * 2, n) - 1);
+            }
         }
     }
 
 
     // Abstract in-place merge
-    public static void merge(int[] arr, int lo, int mid, int hi) {
+    private static void merge(int[] arr, int lo, int mid, int hi) {
 
         // Merge arr[lo..mid] with arr[mid+1..hi]
         int i = lo;      // lo ~ mid
