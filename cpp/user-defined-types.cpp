b6da5a35d786eafccde74f8d9041b8b1acd85c3f
Yoonkwan Kim <kyk0704@hotmail.com>
2016-12-27 22:00:27 -0500

    Create user-defined-types.cpp

---
+++ cpp/user-defined-types.cpp
@@ -0,0 +1,3 @@
+new: allocate memory from an area called the free store (dynamic memory and heap)
+delete: objects are alive until they are destroyed using delete
+Objects allocated on the free store are independent of the scope from which they are created.
